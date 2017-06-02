7b8325b67c3f8e4d95277b607524918d93f2bc7f
renzengpeng <renzengpeng@oasgames.com>
2017-06-02 09:53:36 +0800

    add

---
+++ phpcms/libs/functions/extention.func.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  extention.func.php 用户自定义函数库
+ *
+ * @copyright			(C) 2005-2010 PHPCMS
+ * @license				http://www.healstec.com/license/
+ * @lastmodify			2010-10-27
+ */
+ 
+?>
---
+++ phpcms/modules/upgrade/install/config.inc.php
@@ -0,0 +1,10 @@
+<?php 
+defined('IN_PHPCMS') or exit('Access Denied');
+defined('INSTALL') or exit('Access Denied');
+$module = 'upgrade';
+$modulename = '在线升级';
+$introduce = '在线升级';
+$author = 'phpcms Team';
+$authorsite = 'http://www.healstec.com';
+$authoremail = 'admin@healstec.com';
+?>
